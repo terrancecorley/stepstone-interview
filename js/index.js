89f353ad5ac2846d22b6a51feca0a19b1c19cfbc
Terrance Corley <terrancecorley@outlook.com>
2018-10-24 15:53:25 -0700

    added base styles and styled site header for mobile

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 (() => {
   // placeholder comment
 })();
+
+// ## Project
+
+// Using any language and/or framework of your choosing create an application that allows a user to search The Movie DB's API (https://www.themoviedb.org/documentation/api).
+
+// ### Requirements:
+//  - The app should output a single page to the user with a search bar and the results listed below.
+//  - Result Movie Posters should be displayed in a grid as the results.
+//  - Only 10 results need to be displayed, there is no need for pagination.
+//  - Publish your finished project on Github and email a link to jcarlos@stepstoneglobal.com
