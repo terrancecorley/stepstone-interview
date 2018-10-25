f68a71e428d679d14730ade4cbc926a9bc33091b
Terrance Corley <terrancecorley@outlook.com>
2018-10-25 11:48:43 -0700

    added poster GET functionality

--- js/index.js
+++ js/index.js
@@ -3,22 +3,34 @@
   const submitBtn = document.querySelector('#poster-search-btn');
   const apiKey= config.MOVIE_DB_KEY;
   const baseURL = 'https://api.themoviedb.org/3';
+  const basePosterPath = 'https://image.tmdb.org/t/p/w500';
 
   submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
     let movieInput = document.querySelector('#poster-search-input').value;
+    let movieList = document.querySelector('#poster-search-results');
+    let movies = '';
+
     fetch(`${baseURL}/search/movie?api_key=${apiKey}&query=${movieInput}`)
-      .then(res => console.log(res.json()));
+      .then(res => res.json())
+      .then(res => res.results)
+      .then(posters => posters.slice(0, 10))
+      .then(posters => posters.map((poster, index) => {
+        if (poster.poster_path === null) {
+          posters.splice(index, 1);
+        } else {
+          let movieSrc = basePosterPath + poster.poster_path;
+          let movieAlt = `${movieInput} movie poster`;
+          movies += `
+            <li>
+              <img
+                src=${movieSrc}
+                alt=${movieAlt} />
+            </li>
+          `;
+        }
+      }))
+      .then(() => movieList.innerHTML = movies);
   });
 
 })();
-
-// ## Project
-
-// Using any language and/or framework of your choosing create an application that allows a user to search The Movie DB's API (https://www.themoviedb.org/documentation/api).
-
-// ### Requirements:
-//  - The app should output a single page to the user with a search bar and the results listed below.
-//  - Result Movie Posters should be displayed in a grid as the results.
-//  - Only 10 results need to be displayed, there is no need for pagination.
-//  - Publish your finished project on Github and email a link to jcarlos@stepstoneglobal.com
